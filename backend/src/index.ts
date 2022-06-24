3f3e92a4b3300402f2929ea44693a6693a7096e6
Antonio <antonioadamipalma@gmail.com>
2022-06-23 22:51:57 -0300

    Alterando configurações do cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,14 @@ import './infra/container';
 
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+    }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
